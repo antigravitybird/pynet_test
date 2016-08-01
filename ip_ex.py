7da816553acb01b75282db1b4ac6cb28225d3cc6
Dana <danadonaldson@me.com>
2016-08-01 15:51:53 -0700

    my ip commit

---
+++ ip_ex.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+ip_addr = raw_input ("Input IP Address here: ")
+
+ip_addr = ip_addr.split (".")
+
+print
+print "{:<12} {:<12} {:<12} {:<12}".format(*ip_addr)
+print
+
+
+
+
+
+
