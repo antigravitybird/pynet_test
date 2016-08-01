0b3e561c26e71925864b368039d8099d7564da8a
Dana <danadonaldson@me.com>
2016-08-01 16:44:25 -0700

    Ex 6 Math Commit

---
+++ ex_6_math.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+num1 = int(raw_input ("Input first number here: "))
+num2 = int(raw_input ("Input second number here: "))
+print
+print "Sum: {}".format(num1 + num2)
+print "Difference: {}".format(num1 - num2)
+print "Product: {}".format(num1 * num2)
+print "Quotient: {}".format(num1/float(num2))
+print
+
