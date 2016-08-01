bbb0d3720e3491c30c52139c6cbf351b012ee105
Dana <danadonaldson@me.com>
2016-08-01 14:54:27 -0700

    my commit

---
+++ ex_3_names.py
@@ -0,0 +1,12 @@
+name1 = "Minnie"
+name2 = "Matix"
+name3 = "Echo"
+name4 = raw_input ("Input fourth name")
+
+print
+print "{:>30} {:>30} {:>30} {:>30}".format(name1, name2, name3, name4)
+print
+
+
+
+
