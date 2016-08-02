52e1c19ea37ce6bc0c454a37eb1d88b1e2f1d35d
Dana <danadonaldson@me.com>
2016-08-02 11:29:24 -0700

    Exercise 14 Dictionary

---
+++ ex_14_dict.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+dict = {'ip': '10.10.8.4', 'username': 'ddonaldson', 'password': 'Password123!', 'vendor': 'Aruba', 'model': 'Tester'}
+print
+print "IP: ", dict['ip']
+print "Username: ", dict['username']
+print "Password: ", dict['password']
+print "Vendor: ", dict['vendor']
+print "Model: ", dict['model']
+print
+
+### New Password Value ###
+dict['password'] = 'Password789!' ##New Password!
+print
+print "dict['password']: ", dict['password']
+print
+
+dict['Secret'] = 'VerySecret'
+
+for key, value in dict.iteritems():
+    print key, value
+
+print "Value : %s" % dict.get('device_type', "cisco_ios")
+
+
+
