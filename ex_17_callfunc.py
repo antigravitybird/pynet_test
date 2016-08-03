e998a0c8929496911323fdf481ca63090b142cab
Dana <danadonaldson@me.com>
2016-08-03 16:38:40 -0700

    Exercise 17 calling functions updated

--- ex_17_callfunc.py
+++ ex_17_callfunc.py
@@ -3,6 +3,8 @@
 def my_func(x, y, z=20):
     return x + y + z
 
+def my_func2(x, y, z=20):
+    return x, y, z
 
 print
 print "Calling with three positional arguments: "
@@ -15,9 +17,13 @@ print "Calling with one positional argument and two named arguments: "
 print "Value: ", my_func(10, y=15, z=55)
 print
 print "Calling with three strings: "
-print "Value: ", my_func(x='happy', y='little', z='string')
+print "Value: ", my_func(x='happy ', y='little ', z='string')
 print
-print "Calling with three lists: {}".format(" ", " ", " ")
-print "Value: ", my_func(x=['water', 'ice', 'snow'], y=['lava', 'fire', 'steam'], z=['grass', 'soil', 'flora'])
+print "Calling with three lists: "
+print "Value: ", my_func2(x=['water', 'ice', 'snow'], y=['lava', 'fire', 'steam'], z=['grass', 'soil', 'flora'])
+print
+print "Making that call more readable: "
+ret1, ret2, ret3 = my_func2(x=['water', 'ice', 'snow'], y=['lava', 'fire', 'steam'], z=['grass', 'soil', 'flora'])
+print " {}\n {}\n {}".format(ret1, ret2, ret3)
 print
 
